c3178a40e1c9c471c1bb528981cd53b47d829677
Matthias Himmelsbach <matthias.himmelsbach@gmail.com>
2024-10-27 08:15:50 +0100

    created prettierrc

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,16 @@
-function ToDoList() {
-    return <div className={'w-1/4 bg-amber-300 m-auto'}>ToDoList</div>
+import Button from './Button.tsx';
+
+interface Props {
+  title?: string;
 }
 
-export default ToDoList
+function ToDoList({ title = 'Default Title' }: Props) {
+  return (
+    <div className={'w-1/2 bg-amber-300 m-auto p-4 rounded'}>
+      <h1 className={'text-2xl m-auto'}>{title}</h1>
+      <Button>asd</Button>
+    </div>
+  );
+}
+
+export default ToDoList;
